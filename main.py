d0cd0e0dcf85ca55d62e5bba9eaa610d3cdc2a90
KevStrasser <strasserk@outlook.de>
2026-07-30 12:34:07 +0200

    Update2

--- main.py
+++ main.py
@@ -3,16 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def read_csv(file_path: str) -> DataFrame:
+def read_csv(file_path: str) -> pd.DataFrame:
     df: pd.DataFrame = pd.read_csv(file_path)
     df["Label"] = ""                    # Create new column for Labeling
     return df
 
 def label_data(df: pd.DataFrame) -> pd.DataFrame:
-    #for _ in range(len(df["velocity"])):
-    #    if df["velocity"][_] >= 120:
-    #        df.loc[_, "Label"] = "120+"
-
     df.loc[df["velocity"] >= 120, "Label"] = "120+"
     return df
 
@@ -23,57 +19,32 @@ def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 def matching_trips(df: pd.DataFrame) -> list:
-    list_of_120plus = []
-
-    '''
-    for index in range(len(df)):
-        if df["Label"][index] == "120+":
-            #print(df.iloc[index]["vehicleId"])
-            #print(df.iloc[index]["tripId"])
-
-            if not list_of_120plus:
-                list_of_120plus.append(df.iloc[index]["tripId"])
-
-            elif list_of_120plus[-1] != df.iloc[index]["tripId"]:
-                list_of_120plus.append(df.iloc[index]["tripId"])
-    '''
-
     trips = (
         df.loc[df["velocity"] >= 120, "tripId"]
         .drop_duplicates()
         .tolist()
     )
-
     print("tripIds for >= 120 km/h calculation:", trips[0], "and", trips[1])
 
     return trips
 
 def draw_plot(df: pd.DataFrame) -> None:
     # Get Data
-    time_list = []
-    velocity_list = []
-    temperatureRotorBack_list = []
-
-    for index in range(0, len(df)):
-        velocity_list.append(df.iloc[index]["velocity"])
-        temperatureRotorBack_list.append(df.iloc[index]["temperatureRotorBack"])
-
-    # Prepare Data
-    for index in range(len(velocity_list)):
-        time_list.append(index)
-
+    velocity = df["velocity"]
+    temperature = df["temperatureRotorBack"]
+    time = (df["timeUnix"] - df["timeUnix"].iloc[0]) / 1000
 
     # Plot Data for Velocity and TemperatureRotorBack in one Graph
     plt.figure(figsize=(20, 10))
-    plt.plot(time_list,
-             velocity_list,
+    plt.plot(time,
+             velocity,
              marker=".",
              linestyle="-",
              color="b",
              label="Velocity"
              )
-    plt.plot(time_list,
-             temperatureRotorBack_list,
+    plt.plot(time,
+             temperature,
              marker=".",
              linestyle="-",
              color="r",
@@ -96,7 +67,6 @@ def draw_plot(df: pd.DataFrame) -> None:
     plt.show()
 
 def calc_average_temp(df: pd.DataFrame) -> None:
-
     greater = df[df["velocity"] >= 120]
     lower = df[df["velocity"] < 120]
 
@@ -106,7 +76,6 @@ def calc_average_temp(df: pd.DataFrame) -> None:
 
 
 def calc_correlation(df: pd.DataFrame) -> None:
-
     correlation_back = df["velocity"].corr(df["temperatureRotorBack"])
     correlation_front = df["velocity"].corr(df["temperatureRotorFront"])
 
@@ -115,33 +84,23 @@ def calc_correlation(df: pd.DataFrame) -> None:
 
 
 def create_histograms(df: pd.DataFrame) -> None:
-    temp_vel_greater120_list = []
-    temp_vel_lower120_list = []
+    # Create Histogram Data
+    greater = df.loc[df["velocity"] >= 120, "temperatureRotorBack"]
+    lower = df.loc[df["velocity"] < 120, "temperatureRotorBack"]
 
-    for index in range(len(df)):
-        if df["Label"][index] == "120+":
-            temp_vel_greater120_list.append(df.loc[index, "temperatureRotorBack"])
-
-        else:
-            temp_vel_lower120_list.append(df.loc[index, "temperatureRotorBack"])
-
-    #temp_vel_greater120_list = df.loc[df["velocity"] >= 120, "temperatureRotorBack"]
-    #temp_vel_lower120_list = df.loc[df["velocity"] < 120, "temperatureRotorBack"]
-
-
-    hist_data = {
-        "TemperatureRotorBackAll_greater": temp_vel_greater120_list,
-        "TemperatureRotorBackAll_lower": temp_vel_lower120_list
-    }
-
-    all_values = hist_data["TemperatureRotorBackAll_greater"] + hist_data["TemperatureRotorBackAll_lower"]
     cluster_size = 5
+    all_values = pd.concat([greater, lower])
 
-    bins = np.arange(min(all_values) - (min(all_values) % cluster_size), max(all_values) + cluster_size, cluster_size)
+    bins = np.arange(
+        all_values.min() - all_values.min() % cluster_size,
+        all_values.max() + cluster_size,
+        cluster_size
+    )
+
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5), sharey=True)
 
-    # Linker Graph: Temp1
-    ax1.hist(hist_data["TemperatureRotorBackAll_greater"], bins=bins, color='skyblue', edgecolor='black', alpha=0.8)
+    # Left Graph: Temp RotorBack for velocity >= 120 km/h
+    ax1.hist(greater, bins=bins, color='skyblue', edgecolor='black', alpha=0.8)
     ax1.set_title("Histogram: temperatureRotorBack for velocity ≥ 120 km/h", fontweight='bold')
     ax1.set_xlabel("Temperatur in [°C]")
     ax1.set_ylabel("Frequency")
@@ -149,8 +108,8 @@ def create_histograms(df: pd.DataFrame) -> None:
     ax1.tick_params(axis='x', rotation=45)
     ax1.grid(axis='y', linestyle='--', alpha=0.5)
 
-    # Rechter Graph: Temp2
-    ax2.hist(hist_data["TemperatureRotorBackAll_lower"], bins=bins, color='lightcoral', edgecolor='black', alpha=0.8)
+    # Right Graph: Temp RotorBack for velocity < 120 km/h
+    ax2.hist(lower, bins=bins, color='lightcoral', edgecolor='black', alpha=0.8)
     ax2.set_title("Histogram: temperatureRotorBack for velocity < 120 km/h", fontweight='bold')
     ax2.set_xlabel("Temperatur in [°C]")
     ax2.set_xticks(bins)
@@ -190,13 +149,6 @@ if __name__ == '__main__':
 
     # Find matching Vehicles and Trips
     trips_120plus = matching_trips(labeled_data)
-
-    '''
-    #Create Groups and group by vehicleId                      <- Maybe useless
-    vehicles = labeled_data.groupby("vehicleId")
-    vehicle_name = list(vehicles.groups.keys())
-    vehicle1 = vehicles.get_group(vehicle_name[5])
-    '''
 
     # Create Groups and group by tripId
     trips = labeled_data.groupby("tripId")
